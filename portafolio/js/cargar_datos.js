7911c87cfa21fafb9c457d280a0b6a75a63a3be3
Sebastian Quiros Esquivel <se13.quiros@gmail.com>
2026-01-24 23:35:47 -0600

    Delete portafolio directory

--- portafolio/js/cargar_datos.js
+++
@@ -1,9 +0,0 @@
-async function cargarJSON(url) {
-    try {
-        const respuesta = await axios.get(url);
-        return respuesta.data;
-    } catch (error) {
-        console.error("Error al cargar el JSON:", error);
-        return []; // Retorna un arreglo vacío si hay error
-    }
-}
