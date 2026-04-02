855656783dd9c00a773f271d043d67423db632cb
Sebastian Quiros Esquivel <se13.quiros@gmail.com>
2026-04-02 16:44:05 -0600

    Delete portafolio directory

--- portafolio/js/certificados.js
+++
@@ -1,7 +0,0 @@
-async function mostrarCertificados(){
-    const certificados = await cargarJSON('../json/certificados.json'); // obtenemos los datos
-
-    for (let certificado of certificados){
-        $('#contenedorCertificaciones').append(`<a href="${certificado.urlImagen}" target="_blank"><img src="${certificado.urlImagen}" alt="certificado"></a>`)
-    }
-}
